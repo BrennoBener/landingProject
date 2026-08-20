122ba3479c109fdbb01522f67a0cfaf19ce568ed
Brenno Bener <brennobener@outlook.com>
2026-08-20 16:30:59 -0300

    Testando como adicionar novos produtos

--- config.js
+++ config.js
@@ -25,6 +25,20 @@ const dadosCliente = {
                 "modelos/conjunto-costas.jpg"
             ],
             whatsapp: "5585998097181" 
+        },
+
+        {
+            nome: "Conjunto Alfaiataria",
+            ref: "PI-042",
+            desc: "Conjunto blaser e calça com corte reto. Alta saída, ideal para compor vitrines.",
+            preco: "R$ 119,90",
+            especificacoes: "Tamanhos: P, M, G | Cores: Fúcsia, Preto",
+            fotoPrincipal: "modelos/modelo1.png",
+            galeria: [
+                "modelos/modelo1.png", 
+                "modelos/modelo2.png"
+            ],
+            whatsapp: "5585998097181" 
         }
     ]
 };
